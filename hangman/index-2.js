8f4d0fb50e8f55e411b1e9ff6f37fd30c489d9ee
Arin Ray <arinrayrusho@gmail.com>
2024-11-04 22:16:32 +0530

    Minor changes

--- hangman/index-2.js
+++ hangman/index-2.js
@@ -1,3 +1,12 @@
+var wrongGuessCount = 0;
+
+const cleanUp = () => {
+    document.removeEventListener("keydown", handleKeydown);
+    Array.from(document.getElementsByClassName("key")).forEach(key => {
+        key.removeEventListener("click", key.f);
+    })
+}
+
 const getData = async () => {
     const wordSize = Math.floor(Math.random() * 5 + 5);
     const url = `https://random-word-api.herokuapp.com/word?length=${wordSize}`;
@@ -11,9 +20,23 @@ const getData = async () => {
         const word = json[0];
         console.log(word)
 
-        Array.from(word).forEach((letter) => {
-            document.getElementById('rec2').innerHTML += getLetterHTMLString(letter.toUpperCase());
+        Array.from(word).forEach((letter, i) => {
+            setTimeout(() => {
+                document.getElementById('rec2').innerHTML += getLetterHTMLString(letter.toUpperCase());
+            }, 300 * i);
         })        
+
+        setTimeout(() => {
+            Array.from(document.getElementsByClassName('guessed')).forEach(key => key.classList.remove('guessed'));
+        }, 300 * wordSize);
+        
+        setTimeout(() => {            
+            Array.from(document.getElementsByClassName("key")).forEach(key => {
+                key.addEventListener("click", key.f = () => {handleKeyInput(key.id)});
+            })
+    
+            document.addEventListener("keydown", handleKeydown);
+        }, 300 * wordSize + 400);
     } catch (error) {
         console.error(error.message);
     }
@@ -31,6 +54,19 @@ const getLetterHTMLString = (letter) => {
 const handleKeyInput = (key) => {
     const letters = Array.from(document.getElementsByClassName(`letter-${key}`))
     if (letters.length === 0) {
+        wrongGuessCount++;
+        document.getElementById(`hangman-piece-${wrongGuessCount}`).classList.remove("hidden-hangman-pieces");
+        if (wrongGuessCount === 6) {
+            console.log("LOSS!");
+            Array.from(document.getElementsByClassName('hidden')).forEach(hiddenItem => {
+                hiddenItem.classList.remove('hidden');
+            })
+            Array.from(document.getElementsByClassName('letter-container')).forEach(letterContainer => {
+                letterContainer.classList.add('loss-red');
+            })
+            cleanUp();
+            return;
+        }
         console.log('WRONG GUESS!');
         Array.from(document.getElementsByClassName('blank')).forEach(blank => {
             blank.classList.add('red');
@@ -55,13 +91,10 @@ const handleKeyInput = (key) => {
 
         if ((Array.from(document.getElementsByClassName('hidden'))).length === 0) {
             console.log("WIN!");
-            Array.from(document.getElementsByClassName('letter-container')).forEach(blank => {
-                blank.classList.add('win-green');
+            Array.from(document.getElementsByClassName('letter-container')).forEach(letterContainer => {
+                letterContainer.classList.add('win-green');
             })
-            document.removeEventListener("keydown", handleKeydown);
-            Array.from(document.getElementsByClassName("key")).forEach(key => {
-                key.removeEventListener("click", key.f);
-            })
+            cleanUp();
         }
     }
 }
@@ -75,9 +108,4 @@ const handleKeydown = (event) => {
     }
 };
 
-Array.from(document.getElementsByClassName("key")).forEach(key => {
-    key.addEventListener("click", key.f = () => {handleKeyInput(key.id)});
-})
-
-document.addEventListener("keydown", handleKeydown);
 getData();
